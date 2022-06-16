69f2904956e33c96711eac14d01e6372af7904c4
Werve <13850028+Werve@users.noreply.github.com>
2022-06-16 18:52:21 +0200

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -3,5 +3,5 @@ ui_print "- Simple Disabling forceencrypt on Project Treble device -";
 find /system/vendor/etc -name "fstab.*" -exec sed -i 's/forceencrypt/encryptable/g' {} ;
 find /system/vendor/etc -name "fstab.*" -exec sed -i 's/forcefdeorfbe/encryptable/g' {} ;
 find /system/vendor/etc -name "fstab.*" -exec sed -i 's/fileencryption/encryptable/g' {} ;
-find /system/vendor/etc -name "fstab.*" -exec cp '{}' ../system/vendor/ \;
+find /system/vendor/etc -name "fstab.*" -exec cp '{}' system/vendor/etc/ \;
 ui_print "- Done. Check after reboot if the fstab.* file in /vendor/etc don't have forceencrypt string-";
